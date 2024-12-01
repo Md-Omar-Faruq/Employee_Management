d5fe460993387fc70e17ee8d603745a99f9a6ab4
MD. OMAR FARUQ <57213561+Md-Omar-Faruq@users.noreply.github.com>
2024-12-01 23:33:01 +0600

    Add Backend Project

---
+++ employeeManagement/src/main/java/com/employee/controller/test/TestController.java
@@ -0,0 +1,17 @@
+package com.employee.controller.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> greetings(){
+        return ResponseEntity.ok("Welcome To Employee Management:");
+    }
+
+}
